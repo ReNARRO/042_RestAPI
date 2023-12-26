39ddae21cce4cd1adefc695abaa43691ce7a558d
E Dewobroto <e.dewobroto.ft21@mail.umy.ac.id>
2023-12-26 13:15:56 +0700

    Membuat home view model

---
+++ app/src/main/java/com/example/cosumerestapi/ui/view_model/HomeViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.cosumerestapi.ui.view_model
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cosumerestapi.modeldata.Kontak
+import com.example.cosumerestapi.repositori.KontakRepository
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+
+sealed class KontakUIState {
+    data class Success(val kontak: List<Kontak>) : KontakUIState()
+    object Error : KontakUIState()
+    object Loading : KontakUIState()
+}
+
+class HomeViewModel(private val kontakRepository: KontakRepository): ViewModel(){
+    var kontakUIState: KontakUIState by mutableStateOf(KontakUIState.Loading)
+
+
+    init {
+        getKontak()
+    }
+
+    fun getKontak(){
+        viewModelScope.launch {
+            kontakUIState = KontakUIState.Loading
+            kontakUIState = try {
+                KontakUIState.Success(kontakRepository.getKontak())
+            }catch (e: IOException){
+                KontakUIState.Error
+            }catch (e: HttpException){
+                KontakUIState.Error
+            }
+        }
+    }
+}
