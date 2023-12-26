35bfcab6213c7432290d1698f2d434d25a51d5ca
E Dewobroto <e.dewobroto.ft21@mail.umy.ac.id>
2023-12-26 20:59:11 +0700

    Membuat file insert view model

---
+++ app/src/main/java/com/example/cosumerestapi/ui/view_model/InsertViewModel.kt
@@ -0,0 +1,58 @@
+package com.example.cosumerestapi.ui.view_model
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cosumerestapi.modeldata.Kontak
+import com.example.cosumerestapi.repositori.KontakRepository
+import kotlinx.coroutines.launch
+
+class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent) {
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertKontak() {
+        viewModelScope.launch {
+            try {
+                kontakRepository.insertKontak(insertKontakState.insertUiEvent.toKontak())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
+data class InsertUiEvent(
+    val id: Int = 0,
+    val nama: String = "",
+    val email: String = "",
+    val nohp: String = ""
+)
+
+fun InsertUiEvent.toKontak() : Kontak = Kontak(
+    id = id,
+    nama = nama,
+    email = email,
+    nohp = nohp
+)
+
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
+
+fun Kontak.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    id = id,
+    nama = nama,
+    email = email,
+    nohp = nohp
+)
+
+fun Kontak.toUiStateKontak(): InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent()
+)
