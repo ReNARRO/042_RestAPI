fb87d00712195c4d7e54626c0813fce69a5f3363
E Dewobroto <e.dewobroto.ft21@mail.umy.ac.id>
2023-12-26 13:35:53 +0700

    membuat function top app bar

--- app/src/main/java/com/example/cosumerestapi/ui/KontakApp.kt
+++ app/src/main/java/com/example/cosumerestapi/ui/KontakApp.kt
@@ -45,3 +45,27 @@ fun KontakApp(
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBarKontak(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = ""
+                    )
+                }
+            }
+        }
+    )
+}
